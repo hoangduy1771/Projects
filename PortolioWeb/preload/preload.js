8b6c49275f5607594c59ac66e0bae28bd4e203fb
Hoàng Duy <hoangduy1771@gmail.com>
2020-12-07 04:08:47 +0700

    Done Preload Page

---
+++ PortolioWeb/preload/preload.js
@@ -0,0 +1,21 @@
+// loader percent count
+const percentNum = document.querySelector(".progress")
+const preloadText = document.querySelector(".preload-text")
+let counter = 0;
+setInterval(() => {
+    if(counter == 100){
+        clearInterval();
+    }else {
+        counter += 1;
+        percentNum.textContent = counter + "%";
+    }
+}, 25)
+
+// replace reload text
+setTimeout(() =>{
+    // preloadText.textContent = "Drinking coffee"
+    // setTimeout(() => {
+        preloadText.textContent = "Drinking Coffee"
+    // }, 2000)
+}, 2500)
+
