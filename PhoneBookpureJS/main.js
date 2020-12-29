f30466b183ff1f33b58c75cd47370d4ae6a1418e
hoang duy <hoangduy1771@gmail.com>
2020-12-30 02:16:50 +0700

    Edit text

--- PhoneBookpureJS/main.js
+++ PhoneBookpureJS/main.js
@@ -1,5 +1,5 @@
 /**
- * Sử dụng kiến thức đã học, tạo ra một ứng dụng danh bạ điện thoại, có các chức năng:
+ * Một ứng dụng danh bạ điện thoại, có các chức năng:
  * - Nhập dữ liệu contact (name, phone number)
  * - Sửa dữ liệu contact
  * - Xoá contact
